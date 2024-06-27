1ae528ca535e762975124d1fccbb4e2c595e4022
Kobic oyd11 <kcalev@gmail.com>
2024-06-27 17:36:22 +0200

    bugfix, start server script in docker

--- index_all.sh
+++ index_all.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+PORT=${1:-5001}
+
 for f in input_audio/*.opus; do
 	echo $f
-	curl http://127.0.0.1:5000/index -F file=@"$f"
+	curl http://127.0.0.1:$PORT/index -F file=@"$f"
 	echo -----
 done
 
