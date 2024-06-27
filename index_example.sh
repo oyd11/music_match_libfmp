823ae4058e1d7a4d5fa2bec89959af6833063fef
Kobic oyd11 <kcalev@gmail.com>
2024-06-27 05:13:09 +0200

    indexing example script

---
+++ index_example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PORT=${1:-5001}
+
+curl http://127.0.0.1:$PORT/index -F file=@'input_audio/Gnarls Barkley - Crazy (Official Video) [4K Remaster] [-N4jf6rtyuw].opus'
+
+curl http://127.0.0.1:$PORT/index -F file=@'input_audio/Boogie Down Productions - My Philosophy (Official HD Video) [h1vKOchATXs].opus'
+
+
+
+
---
+++ mk_venv.sh
@@ -0,0 +1,9 @@
+# dot me
+
+dirname=$(basename `pwd`)
+echo $dirname
+
+python3.10 -m venv ~/.venv/$dirname
+
+
+
