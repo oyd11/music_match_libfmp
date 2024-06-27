28ce23befe744744b90d30f191c67253f23381a2
Kobic oyd11 <kcalev@gmail.com>
2024-06-27 17:55:33 +0200

    logging

--- run_server.sh
+++ run_server.sh
@@ -14,6 +14,13 @@ cd $BASE_FOLDER
 
 . ./venv.sh
 
+if [ -d $HOTSWAP_FOLDER ]; then
+	echo "Logging to Hotswap folder: $HOTSWAP_FOLDER"
+	LOG_FOLDER=$HOTSWAP_FOLDER
+else
+	LOG_FOLDER=$BASE_FOLDER
+fi # hotswap
+
 if [ ! -d $UPLOADS_FOLDER ]; then
 	if [ -d $HOTSWAP_FOLDER ]; then
 		echo "Using Hotswap folder: $HOTSWAP_FOLDER"
@@ -27,10 +34,10 @@ mkdir -p $UPLOADS_FOLDER/query || exit 1
 
 if [ "$DEV" == "dev" ]; then
 	echo Development mode
-	python3 http_app_audio/http_app_audio.py | tee -a http_app_audio.dev.log
+	python3 http_app_audio/http_app_audio.py |& tee -a $LOG_FOLDER/app_audio.dev.log
 else
 	echo Release mode, using gunicorn:
-	gunicorn -w 1 -b 0.0.0.0:5000 http_app_audio.http_app_audio:app   | tee -a http_app_audio.log
+	gunicorn -w 1 -b 0.0.0.0:5000 http_app_audio.http_app_audio:app |& tee -a $LOG_FOLDER/app_audio.log
 
 	# More than one worker only after we have shared indexing between threads..
 	# gunicorn -w 4 -b 0.0.0.0:5000 http_app_audio.http_app_audio:app
